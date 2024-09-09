ff14b0a0010cd4868ae8ea8f6cfb23d86bcd6da8
Pashtet <ptashkaaaaaa@gmail.com>
2024-09-09 20:36:53 +0200

    feat: add basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import(
+        './periodicTableFeature/pages/table-page/table-page.component'
+      ).then((m) => m.TablePageComponent),
+  },
+];
