aeb859db97b9bb564c526871e8e8f3acdf519ad1
Pashtet <ptashkaaaaaa@gmail.com>
2024-09-12 14:20:49 +0200

    feat: create periodicFilter component

---
+++ src/app/periodicTableFeature/components/periodic-table-filter/periodic-table-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-periodic-table-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './periodic-table-filter.component.html',
+  styleUrl: './periodic-table-filter.component.scss'
+})
+export class PeriodicTableFilterComponent {
+
+}
