9153f4bb1ce7631bff4bf2f7e52ddf4fb0ec6e99
Pashtet <ptashkaaaaaa@gmail.com>
2024-09-12 14:13:56 +0200

    fix: change table page path in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import(
-        './periodicTableFeature/pages/table-page/table-page.component'
-      ).then((m) => m.TablePageComponent),
+    loadComponent: () => import(
+      './periodicTableFeature/pages/tablePage/tablePage.component'
+    ).then((m) => m.TablePageComponent),
   },
 ];
