7dd9b79fd2af55140eb5e56fb888e2902a5796a4
Pashtet <ptashkaaaaaa@gmail.com>
2024-09-12 14:23:09 +0200

    fix: change component name in imports

--- src/app/periodicTableFeature/pages/tablePage/tablePage.component.ts
+++ src/app/periodicTableFeature/pages/tablePage/tablePage.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 
 import { PeriodicTableComponent } from '../../components/periodic-table/periodic-table.component';
-import { PrerodicTableFilterComponent } from '../../components/prerodic-table-filter/prerodic-table-filter.component';
+import { PeriodicTableFilterComponent } from '../../components/periodic-table-filter/periodic-table-filter.component';
 
 @Component({
   selector: 'app-table-page',
   standalone: true,
   imports: [
     MatButton,
+    PeriodicTableFilterComponent,
     PeriodicTableComponent,
-    PrerodicTableFilterComponent,
   ],
   templateUrl: './tablePage.component.html',
   styleUrl: './tablePage.component.scss'
