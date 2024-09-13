166b7bc7876b599084990db7823eafaa60680d44
Pashtet <ptashkaaaaaa@gmail.com>
2024-09-13 21:23:21 +0200

    feat: add PeriodicTableComponent possibility to open dialog and change table elements

--- src/app/periodicTableFeature/components/periodic-table/periodic-table.component.ts
+++ src/app/periodicTableFeature/components/periodic-table/periodic-table.component.ts
@@ -1,11 +1,20 @@
 import {
-  AfterViewInit, Component, OnInit, ViewChild
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+  ViewChild,
 } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTable, MatTableModule } from '@angular/material/table';
 
 import { FilterFormEmitterService } from '../../services/filter-form-emitter.service';
+import { EditElementDialogComponent } from '../edit-element-dialog/edit-element-dialog.component';
 import { PeriodicTableColumnNames, PeriodicTableDataSource, PeriodicTableItem } from './periodic-table-datasource';
 
 @Component({
@@ -13,25 +22,41 @@ import { PeriodicTableColumnNames, PeriodicTableDataSource, PeriodicTableItem } 
   templateUrl: './periodic-table.component.html',
   styleUrl: './periodic-table.component.scss',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule]
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PeriodicTableComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<PeriodicTableItem>;
-  dataSource!: PeriodicTableDataSource;
 
-  displayedColumns:PeriodicTableColumnNames[] = ['position', 'name', 'weight', 'symbol'];
+  private readonly dialog = inject(MatDialog);
+  private readonly filterFormEmitter = inject(FilterFormEmitterService);
 
-  constructor(private readonly filterFormEmitter: FilterFormEmitterService) {
-  }
+  private destroyRef = inject(DestroyRef);
+
+  protected readonly displayedColumns:PeriodicTableColumnNames[] = ['position', 'name', 'weight', 'symbol'];
+  private dataSource!: PeriodicTableDataSource;
 
   ngOnInit(): void {
-    this.filterFormEmitter.filteredWord$.subscribe();
     this.dataSource = new PeriodicTableDataSource(this.filterFormEmitter.filteredWord$);
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.table.dataSource = this.dataSource;
+  }
+
+  openDialog(rowData:PeriodicTableItem) {
+    const dialogRef = this.dialog.open(EditElementDialogComponent, {
+      data: rowData,
+    });
+
+    dialogRef.afterClosed()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((data:PeriodicTableItem | undefined) => {
+        if (data) {
+          this.dataSource.updateData(data);
+        }
+      });
   }
 }
