5b49e0e6e5ec000a53eed3dbb70eec4c0c40ab3b
Pashtet <ptashkaaaaaa@gmail.com>
2024-09-17 17:20:41 +0200

    fix: change filteredWord$ getter to property

--- src/app/periodicTableFeature/services/filter-form-emitter.service.ts
+++ src/app/periodicTableFeature/services/filter-form-emitter.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class FilterFormEmitterService {
-  private filterWordSubject = new BehaviorSubject('');
+  private filterWordSubject = new Subject<string>();
 
-  get filteredWord$() {
-    return this.filterWordSubject.asObservable();
-  }
+  public filteredWord$ = this.filterWordSubject.asObservable();
 
   public updateData(word:string): void {
     this.filterWordSubject.next(word);
